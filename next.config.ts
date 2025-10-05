52f1f297f80ede8b81460ad1d3c4d0d6d2310bca
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-10-05 02:55:12 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,4 @@
 import type {NextConfig} from 'next';
-import withPWA from "@ducanh2912/next-pwa";
-
-const pwaConfig = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -41,4 +33,4 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['jspdf', 'html2canvas'],
 };
 
-export default pwaConfig(nextConfig);
+export default nextConfig;
