fc829a5336c756e4028a663f56409e87a0b4a39a
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-10-08 01:04:33 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
@@ -87,7 +87,7 @@ export default function LoginPage() {
   });
   
   // Reset form when switching between login and signup
-  React.useEffect(() => {
+  useEffect(() => {
     form.reset({
       email: '',
       password: '',
